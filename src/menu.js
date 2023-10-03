7a3f2bb2800f610264bd6ac43db06989f17b941e
alexsharm <alfex.sharm@gmail.com>
2023-10-03 19:49:30 +0300

    without js

--- src/menu.js
+++
@@ -1,9 +0,0 @@
-function closeMenuFunction() {
-  var element = document.getElementById("backdrop-for-menu");
-  element.classList.add("visually-hidden");
-}
-
-function openMenuFunction() {
-  var element = document.getElementById("backdrop-for-menu");
-  element.classList.remove("visually-hidden");
-}
