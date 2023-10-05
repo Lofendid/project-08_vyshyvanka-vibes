622bbfc1dbeee3875373901184121a63cf0664d8
Lofendid <102857014+Lofendid@users.noreply.github.com>
2023-10-05 16:46:33 +0200

    fix-scroll-when-close-menu

--- src/menu.js
+++ src/menu.js
@@ -11,8 +11,8 @@ document.addEventListener('DOMContentLoaded', function () {
         if (!isClickable) return; // Check if the button is clickable
         isClickable = false; // Disable the button for the delay
         setTimeout(() => {
-            isClickable = true; // Re-enable the button after 1 second
-        }, 500); // 500ms = 0.5 second
+            isClickable = true; // Re-enable the button after 500 milliseconds
+        }, 500); // 500ms = 0.5 seconds
 
         burgerIcon.classList.add('is-open');
         menu.classList.add('is-open');
@@ -30,8 +30,11 @@ document.addEventListener('DOMContentLoaded', function () {
         if (!isClickable) return; // Check if the button is clickable
         isClickable = false; // Disable the button for the delay
         setTimeout(() => {
-            isClickable = true; // Re-enable the button after 1 second
-        }, 500); // 500ms = 0.5 second
+            isClickable = true; // Re-enable the button after 500 milliseconds
+        }, 500); // 500ms = 0.5 seconds
+
+        // Disable scroll-behavior: smooth on the HTML element when closing the menu
+        document.documentElement.style.scrollBehavior = 'auto';
 
         burgerIcon.classList.remove('is-open');
         menu.classList.remove('is-open');
@@ -40,11 +43,23 @@ document.addEventListener('DOMContentLoaded', function () {
         document.body.style.position = '';
         document.body.style.top = '';
         window.scrollTo(0, initialScrollPosition); // Restore the scroll position
+
+        // Re-enable scroll-behavior: smooth after a delay of 500ms
+        setTimeout(() => {
+            document.documentElement.style.scrollBehavior = 'smooth';
+        }, 500); // 500ms = 0.5 seconds
     }
 
     // Function to close the menu when a link is clicked or touched
-    function closeMenuOnClick() {
-        closeMenu();
+    function closeMenuOnClick(event) {
+        // Check if the clicked element is not a menu link
+        if (!event.target.classList.contains('menu-nav-link')) {
+            closeMenu();
+        }
+        else {
+            // Close the menu if a menu link is clicked
+            closeMenu();
+        }
     }
 
     // Add click event listeners
@@ -62,5 +77,3 @@ document.addEventListener('DOMContentLoaded', function () {
         link.addEventListener('touchstart', closeMenuOnClick);
     });
 });
-
-
